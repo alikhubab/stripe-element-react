b2662b072660e010ad2072694abb5cdfd8d616b3
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-08-19 12:19:51 +0500

    implement create payment intent endpoint on the backend

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -1,7 +1,21 @@
 import { useEffect, useState } from "react";
+import {loadStripe} from "@stripe/stripe-js";
 
 function Payment(props) {
-  return (
+    const [stripPromise, setStripPromise] = useState(null);
+    const [clientSecret, setClientSecret] = useState( "");
+
+    useEffect(()=> {
+        fetch('/config').then(async r => {
+            const {publishableKey} = await r.json()
+            console.log(publishableKey)
+            setStripPromise(loadStripe(publishableKey))
+        })
+
+    }, [])
+
+
+    return (
     <>
       <h1>React Stripe and the Payment Element</h1>
     </>
